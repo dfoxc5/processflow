d58350b2bf8e56dd2f29c669c52fbc3266d57724
dfoxc5 <dfox@capitatech.com>
2016-12-28 16:46:52 -0800

    Deploying
    trying to deploy to heroku

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ from app import app
 import os
 port = int(os.environ.get('PORT', 5000))
 
-app.run(debug=True, host='127.0.0.1', port=port)
+# app.run(debug=True, host='127.0.0.1', port=port)
+app.run(host='0.0.0.0', port=port)
