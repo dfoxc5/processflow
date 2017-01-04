d3a654cc3520f1afeedda13e3ccf0cd46cbf32b6
dfoxc5 <dfox@capitatech.com>
2017-01-04 09:50:22 -0800

    Tring to figure out models

--- app/models.py
+++ app/models.py
@@ -2,12 +2,12 @@ from app import db
 
 
 class Roles(db.Model):
-    role_id = db.column(db.Integer, primary_key=True)
+    id = db.column(db.Integer, primary_key=True)
     role_name = db.column(db.String)
 
 
 class Stories(db.model):
-    story_id = db.column(db.Integer, primary_key=True)
+    id = db.column(db.Integer, primary_key=True)
     story_title = db.column(db.String)
     description = db.column(db.String)
     containing_epic = db.column(db.Integer, db.ForeignKey('stories.id'))
