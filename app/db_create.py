e03e05762b10b6f68a88fcc16201af3e4c34b191
dfoxc5 <dfox@capitatech.com>
2017-01-04 12:01:54 -0800

    Fixing db column to none

--- app/db_create.py
+++ app/db_create.py
@@ -25,6 +25,6 @@ def create_roles():
 
 
 def create_test_stories():
-    new_story = models.Stories(story_title="Test 1", description="Test", containing_epic=None, workflow_id=1)
+    new_story = models.Stories(story_title="Test 1", description="Test", containing_epic=None, workflow_id=None)
     db.session.add(new_story)
     db.session.commit()
