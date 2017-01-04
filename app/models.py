826a692f0b6a63ddcf269fa3c0783f4837c6c770
dfoxc5 <dfox@capitatech.com>
2017-01-04 10:08:40 -0800

    Trying to get foriegn keys

--- app/models.py
+++ app/models.py
@@ -10,12 +10,13 @@ class Stories(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     story_title = db.Column(db.String)
     description = db.Column(db.String)
-    containing_epic = db.Column(db.Integer, db.ForeignKey('epics.id'))
+    containing_epic = db.Column(db.Integer, db.ForeignKey('epics.epic_id'))
     contained_stories = db.relationship('Epics', backref='inside_stories', lazy='dynamic')
 
 
 class Epics(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    epic_id = db.Column(db.Integer)
     story_id = db.Column(db.Integer, db.ForeignKey('stories.id'))
 
 
