b2b600fd6f99b0eb2f7405fee66bec37b6d4064b
dfoxc5 <dfox@capitatech.com>
2017-01-05 13:54:07 -0800

    For live app

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 app.secret_key = 'some_secret'
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'testDB.db')
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'testDB.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 from app import views
