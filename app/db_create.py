0dba32b7f3ee8d102ea1d32f1cd9c63794e7bb4c
dfoxc5 <dfox@capitatech.com>
2017-01-09 10:42:02 -0800

    Commenting out dropping the database on load

--- app/db_create.py
+++ app/db_create.py
@@ -10,7 +10,7 @@ from sqlalchemy.schema import (
 
 
 def init_database():
-    drop_everything(db)
+    # drop_everything(db)
     db.create_all()
     roles = models.Roles.query.all()
     if len(roles) is 0:
@@ -40,41 +40,41 @@ def create_test_stories():
     db.session.commit()
 
 
-def drop_everything(db):
-    # From http://www.sqlalchemy.org/trac/wiki/UsageRecipes/DropEverything
-
-    conn = db.engine.connect()
-
-    # the transaction only applies if the DB supports
-    # transactional DDL, i.e. Postgresql, MS SQL Server
-    trans = conn.begin()
-
-    inspector = reflection.Inspector.from_engine(db.engine)
-
-    # gather all data first before dropping anything.
-    # some DBs lock after things have been dropped in
-    # a transaction.
-    metadata = MetaData()
-
-    tbs = []
-    all_fks = []
-
-    for table_name in inspector.get_table_names():
-        fks = []
-        for fk in inspector.get_foreign_keys(table_name):
-            if not fk['name']:
-                continue
-            fks.append(
-                ForeignKeyConstraint((), (), name=fk['name'])
-            )
-        t = Table(table_name, metadata, *fks)
-        tbs.append(t)
-        all_fks.extend(fks)
-
-    for fkc in all_fks:
-        conn.execute(DropConstraint(fkc))
-
-    for table in tbs:
-        conn.execute(DropTable(table))
-
-    trans.commit()
+# def drop_everything(db):
+#     # From http://www.sqlalchemy.org/trac/wiki/UsageRecipes/DropEverything
+#
+#     conn = db.engine.connect()
+#
+#     # the transaction only applies if the DB supports
+#     # transactional DDL, i.e. Postgresql, MS SQL Server
+#     trans = conn.begin()
+#
+#     inspector = reflection.Inspector.from_engine(db.engine)
+#
+#     # gather all data first before dropping anything.
+#     # some DBs lock after things have been dropped in
+#     # a transaction.
+#     metadata = MetaData()
+#
+#     tbs = []
+#     all_fks = []
+#
+#     for table_name in inspector.get_table_names():
+#         fks = []
+#         for fk in inspector.get_foreign_keys(table_name):
+#             if not fk['name']:
+#                 continue
+#             fks.append(
+#                 ForeignKeyConstraint((), (), name=fk['name'])
+#             )
+#         t = Table(table_name, metadata, *fks)
+#         tbs.append(t)
+#         all_fks.extend(fks)
+#
+#     for fkc in all_fks:
+#         conn.execute(DropConstraint(fkc))
+#
+#     for table in tbs:
+#         conn.execute(DropTable(table))
+#
+#     trans.commit()
