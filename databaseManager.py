8b77ffdbcec54c49aa3e02562949adea6c0d876f
dfoxc5 <dfox@capitatech.com>
2017-01-10 15:32:17 -0800

    Trying to get epics to display

--- databaseManager.py
+++ databaseManager.py
@@ -224,15 +224,16 @@ class DatabaseManager:
     def get_containing_epics(self, story_title):
         # if story_title == '':
         #     return []
-        # containing_epics = []
+        containing_epics = []
         # if isinstance(story_title, str):
         #     temp = models.Stories.query.filter(models.Stories.story_title == story_title).first()
         # elif isinstance(story_title, int):
         #     temp = models.Stories.query.filter(models.Stories.id == story_title).first()
-        # containing_epics.append([temp.id, temp.story_title, temp.description, temp.containing_epic])
-        # if self.get_epic(temp.containing_epic) != 0:
-        #     self.get_containing_epics(temp.id)
-        return []
+        temp = models.Stories.query.filter(models.Stories.story_title == story_title).first()
+        containing_epics.append([temp.id, temp.story_title, temp.description, temp.containing_epic])
+        if self.get_epic(temp.containing_epic) != 0:
+            self.get_containing_epics(temp.id)
+        return containing_epics
 
     @staticmethod
     def delete_story(story_id):
